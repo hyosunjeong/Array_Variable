cb5ff3b441709b8a72d69526b17a3dc420ff7400
hyosunjeong <jeong8161@naver.com>
2018-11-15 16:31:21 +0900

    181115

---
+++ src/com/biz/arrays/ScoreArray02.java
@@ -0,0 +1,39 @@
+package com.biz.arrays;
+
+public class ScoreArray02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int intNumbers = 20;
+		
+		int[] intKor = new int[intNumbers];
+		for(int i = 0; i<intNumbers; i++) {
+			intKor[i] = makeScore();
+			intKor[i] = ConstMethod.makeScore();
+		}
+		for(int i = 0; i < intNumbers; i++) {
+			System.out.printf("%d 번 학생 국어점수 : %d\n", (i+1), intKor[i]);
+			
+		}
+		System.out.println("================");
+		int intSum = 0;
+		for(int i = 0; i < intNumbers; i++) {
+			intSum += intKor[i];
+		}
+			
+			System.out.println("총점:");
+			System.out.println(intSum);
+			
+			
+			System.out.println("평균:");
+			System.out.println(intSum/20);
+			System.out.println("------------------");
+	}
+
+	
+	public static int makeScore() {
+		return (int)(Math.random()*(100-50+1))+50;
+		
+	}
+
+	}
---
+++ src/com/biz/arrays/Scores01.java
@@ -0,0 +1,30 @@
+package com.biz.arrays;
+
+public class Scores01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 국어선생님이 어느반 학생 5명의
+		// 국어점수 합계를 계산하고자 한다.
+		// 변수를 선언해서 5명 학생의 국어점수를 
+		// 저장해두고 총점을 계산
+		
+		int intKor1;
+		int intKor2;
+		int intKor3;
+		int intKor4;
+		int intKor5;
+		
+		intKor1 = 100;
+		intKor2 = 85;
+		intKor3 = 70;
+		intKor4 = 65;
+		intKor5 = 80;
+		
+		int intSum = (intKor1 + intKor2 + intKor3 + intKor4 + intKor5);
+		System.out.println("총점: " + intSum);
+		
+
+	}
+
+}
